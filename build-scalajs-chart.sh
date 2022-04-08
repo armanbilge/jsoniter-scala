a94017dfd2bf2c93d947314282b9670b52048d32
Arman Bilge <armanbilge@gmail.com>
2022-04-08 20:26:09 +0000

    Only compare circe and circeJawn

--- build-scalajs-chart.sh
+++ build-scalajs-chart.sh
@@ -1,11 +1,11 @@
 echo 'var providedBenchmarks = ["Chrome", "Edge", "Firefox", "Opera"];
 var providedBenchmarkStore = {
 "Chrome":'
-jq -c . < chrome.json
+jq -c '[ .[] | select( .benchmark | (test("Reading.circe(Jawn)?$"))) ]' < chrome.json
 echo ', "Edge":'
-jq -c . < edge.json
+jq -c '[ .[] | select( .benchmark | (test("Reading.circe(Jawn)?$"))) ]' < edge.json
 echo ', "Firefox":'
-jq -c . < firefox.json
+jq -c '[ .[] | select( .benchmark | (test("Reading.circe(Jawn)?$"))) ]' < firefox.json
 echo ', "Opera":'
-jq -c . < opera.json
+jq -c '[ .[] | select( .benchmark | (test("Reading.circe(Jawn)?$"))) ]' < opera.json
 echo '}'
